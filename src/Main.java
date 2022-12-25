4ac996f5e30335b3b83b552ff4065fb11a000d66
itachi2927 <nguyengiacatluong92@gmail.com>
2022-12-25 20:21:04 +0700

    reverse-stack

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        MyGenericStack a = new MyGenericStack();
+        a.addElement();
+        a.printList();
+        a.reverseList();
+        a.printList();
+    }
+}
